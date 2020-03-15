9ea8d5f1b1130871179ddebdea9b879e9b3a01d9
user <fourleafclover688@gmail.com>
2020-03-15 23:37:08 +0900

    ルーティング整理

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :enemies
   get 'maps/index'
-  get 'maps/player_attack'
-  post 'maps/player_attack'
-  get 'dictionary/index'
-  post 'dictionary/search'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
